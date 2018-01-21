305d18fd17516b726399584c412dc683839cb94d
Chihiro Hasegawa <pgm3rdlinuxor1000@gmail.com>
2018-01-21 11:11:12 +0900

    add GOPATH to mac fish config

--- mac/.config/fish/config.fish
+++ mac/.config/fish/config.fish
@@ -6,17 +6,20 @@ if status --is-login
 	set -x LC_ALL ja_JP.UTF-8
 	set -x EDITOR emacsclient
 	
-	# Ruby Library
+	# Ruby library path
 	set -x RUBYLIB $HOME/all/ctf/tools/pwnlib
 	set -x RUBYLIB $HOME/all/ctf/tools/fsalib:$RUBYLIB
 	set -x RUBYLIB $HOME/all/ctf/tools/libcdb:$RUBYLIB
 	set -x RUBYLIB $HOME/all/ctf/tools/ctf:$RUBYLIB
 
+	# Golang path
+	set -x GOPATH $HOME/.go
+
 	set -x PATH $HOME/all/ctf/tools $PATH
 	set -x PATH $HOME/.rbenv/shims $PATH
 	set -x PATH $HOME/miniconda3/bin $PATH
 	set -x PATH $HOME/.cargo/bin $PATH
-	set -x PATH $HOME/.go $PATH
+	set -x PATH $HOME/.go/bin $PATH
 	
 	# 重複を削除する
 	set -x PATH (echo $PATH | tr ' ' '\n' | sort -ru)
