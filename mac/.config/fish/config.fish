becabdf8d640ae181526fcbaaf32af0909840eb4
owlinux1000 <encry1024@gmail.com>
2019-05-05 20:00:58 +0900

    Update config.fish

--- mac/.config/fish/config.fish
+++ mac/.config/fish/config.fish
@@ -7,7 +7,7 @@ if status --is-login
     set -x LANG ja_JP.UTF-8
     set -x LC_ALL ja_JP.UTF-8
     set -x EDITOR emacsclient
-
+    set -x PATH $HOME/bin $PATH
     # Golang
     set -x GOPATH $HOME/.go
     set -x PATH $HOME/.go/bin $PATH
@@ -18,19 +18,16 @@ if status --is-login
     #set -x RUBYLIB $HOME/all/ctf/tools/libcdb:$RUBYLIB
     #set -x RUBYLIB $HOME/all/ctf/tools/ctf:$RUBYLIB
 
-    # Python
-    set -x PATH $HOME/.miniconda3/bin $PATH
-    source (conda info --root)/etc/fish/conf.d/conda.fish
-
     # Rust
     set -x PATH $HOME/.cargo/bin $PATH
-
     set -x PATH $HOME/all/ctf/tools $PATH
     
     # Removce duplicating PATH
     set -x PATH (echo $PATH | tr ' ' '\n' | sort -ru)
-    
+
     # Ruby
     set -x PATH $HOME/.rbenv/shims $PATH
     rbenv init - | source
+
+    set -x PATH $HOME/.nimble/bin $PATH
 end
